61422d404d9f1c8b1b4bfb8563d603731a20122e
pruchayakulll-svg <pruchayakulll@gmail.com>
2025-11-01 02:26:27 +0700

    Delete database_connect.php

--- database_connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host = "host=localhost"; //host ที่ใช้ในการติดต่อกับ Server
-$port = "port=5432"; //หมายเลข port ที่ใช้ (บางเครื่องอาจจะใช้ 5433หรือเลขอื่น)
-$dbname = "dbname=IoT_gps";
-$credentials = "user=postgres password=postgres";
-
-//โครงสร้างชุดคำสั่งทำสำหรับเชื่อมต่อกับฐานข้อมูล PosgresSQL
-$db=pg_connect("$host $port $dbname $credentials"); 
-?>
